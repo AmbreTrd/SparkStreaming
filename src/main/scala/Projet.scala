b93e12b2ea8c273f6b5611d64db6ef11d0f7fc34
Ambre Tanriverdi <ambre.tanriverdi@gmail.com>
2022-06-30 09:16:13 +0200

    streaming ok
    Division des données à faire avant le code ?

--- src/main/scala/Projet.scala
+++ src/main/scala/Projet.scala
@@ -1,4 +1,6 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.SparkSession
 
 object Projet extends App {
   val sparkSession = SparkSession.builder()
@@ -7,102 +9,40 @@ object Projet extends App {
     .getOrCreate()
   sparkSession.sparkContext.setLogLevel("ERROR")
 
+  sparkSession.conf.set("spark.sql.shuffle.partitions","5") // Du mal à comprendre le shuffle
+
   val transport = sparkSession
-    .read
-    .options(Map("inferSchema"->"true","header"->"true"))
-    .csv("data/Regularities_by_liaisons_Trains_France.csv")
+    .read.format("csv")
+    .option("header","true")
+    .option("inferSchema","true")
+    .load("data/Regularities_by_liaisons_Trains_France.csv")
+  // TODO : Diviser la donnée en amont du lancement du code ?
 
   transport.printSchema()
   transport.show(truncate = false)
 
+  transport.createOrReplaceTempView("retail_table")
 
 
-  """
-  studentData.show()
-
-  studentData.createOrReplaceTempView("retail_tabeul")
-  val tabdf = sparkSession.sql("select InvoiceNo, InvoiceDate from retail_tabeul")
-
-  tabdf.show()
-
-"""
-
-
-  /*
-  val maxPurchase = retailDF.selectExpr(
-  "CustomerID",
-  "(UnitPrice * Quantity) as total_cost",
-  "InvoiceDate")
-  .groupBy(
-    col("CustomerID"),
-    window(col("InvoiceDate"), "1 hour") as "invoiceDate"
-  ).sum("total_cost")
-   */
-  /*
-    System.in.read
-    sparkSession.stop()
-
-    /**
-     * Streaming
-     */
-    // Récuperation le schema static
-    val retailschema = retailDF.schema
-    // Lecture en streaming
-    val retailStream = sparkSession
-      .readStream
-      .schema(retailschema)
-      .format("csv")
-      .option("maxFilesPerTrigger","1")
-      .option("header","true")
-      .load("/Users/yacine/IdeaProjects/ESGI-spark-streaming/data/by-day/*.csv")
-    println("spark is streaming " + retailStream.isStreaming)
-    val maxPurchasePerHour = retailStream.selectExpr(
-      "CustomerID",
-      "(UnitPrice * Quantity) as total_cost",
-      "InvoiceDate")
-      .groupBy(
-        col("CustomerID"),
-        window(col("InvoiceDate"), "1 hour") as "invoiceDate"
-      ).sum("total_cost")
-    // Streaming Action
-    maxPurchasePerHour.writeStream
-      .format("memory") // memory = store in-memory table
-      .queryName("customer_table")  // the name of the in-memory table
-      .outputMode("complete") // complete = all the counts should be in the table
-      .start
-    println(sparkSession.streams.active)
-    for (i <- 1 to 50) {
-      sparkSession.sql(
-        """
-          |Select * from customer_table
-          |ORDER BY `sum(total_cost)` DESC
-          |""".stripMargin
-      ).show(false)
-      Thread.sleep(1000)
-    }
-    System.in.read
-    sparkSession.stop()
-    */
+  /**
+   * Streaming
    */
 
-  /* POUR CONCATENER DEUX DATAFRAMES D'UN STREAM
+  // Récupérer le schema static
 
-  val impressions = sparkSession
+  val transportSchema = transport.schema
+
+  // Lecture en streaming
+
+  val transportStream = sparkSession
     .readStream
-    .format("rate") // rate génere des données en streaming avec deux colonnes:
-                            // col1 = value : incrémental (0,1,2....)
-                            // col2 = timestamp : temps de la génération de la données
-                            // | value | timestamp |
-                            // ---------------------
-                            // | 0     | 01-06-2022 14:14:32-234 |
-    .option("rowsPerSecond", "5")  // genere 5 lignes par seconde
-    .option("numPartitions", "1") // nombre de partitions du DF
-    .load()
-    .select(
-      col("value").as("adId"),
-      col("timestamp").as("impressionTime")
-    )
-   */
+    .schema(transportSchema)
+    .format("csv")
+    .option("maxFilesPerTrigger","1")
+    .option("header","true")
+    .load("data/Regularities_by_liaisons_Trains_France.csv")
+
+  println("Spark is streaming " + transportStream.isStreaming)
 
 
 }
